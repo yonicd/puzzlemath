1216db1de53d22387b525693c4b355985ed76116
Jonathan Sidi <yonis@metrumrg.com>
2020-12-11 22:48:55 -0500

    use reactor instead of crrry in deploy reactor skip check

--- tests/testthat/reactor-crrry.R
+++ tests/testthat/reactor-crrry.R
@@ -2,23 +2,44 @@ testthat::context("testing reactivity")
 
 # We run a test with the expectation that the hist tag will be triggered once.
 
-testthat::describe('reactivity',{
-
-  test <- crrry::CrrryProc$new(
-    chrome_bin = pagedown::find_chrome(),
-    shiny_port = httpuv::randomPort(),
-    chrome_port = httpuv::randomPort(),
-    fun = "puzzlemath::run_app()",
-    pre_launch_cmd = glue::glue("whereami::set_whereami_log('{tempdir()}')"),
-    headless = FALSE
+driver_commands <- quote({
+  
+  # wait for input$n element to be created
+  el_game <- reactor::wait(
+    test_driver = test_driver,
+    expr = test_driver$client$findElement(using = 'id', value = 'game')
   )
   
-  test$wait_for_shiny_ready()
-  hist_counter <- reactor::read_reactor(tempdir())
-  test$stop()
+  el_game$clickElement()
+  
+  el_draw <- reactor::wait(
+    test_driver = test_driver,
+    expr = test_driver$client$findElement(using = 'id', value = 'draw')
+  )
+  
+  el_draw$clickElement()
+  
+})
+
+testthat::describe('reactivity',{
+
+  click_counter <- reactor::test_reactor(
+    test_port = 3456,
+    expr          = driver_commands,
+    test_driver   = reactor::firefox_driver(),
+    processx_args = c(
+      reactor::runApp_args()[-4],
+      glue::glue("options('shiny.port'= 3456,shiny.host='0.0.0.0')"),
+      'puzzlemath::run_app()'
+    )
+  )
   
   it('reactivity at startup',{
-    reactor::expect_reactivity(object = hist_counter, tag = 'plot',count =  1)
+    reactor::expect_reactivity(object = click_counter, tag = 'plot',count =  1)
+  })
+  
+  it('reactivity first draw',{
+    reactor::expect_reactivity(object = click_counter, tag = 'draw',count =  4)
   })
   
 })
